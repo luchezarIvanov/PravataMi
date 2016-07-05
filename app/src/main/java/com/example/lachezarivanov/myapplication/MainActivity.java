d5a8c71b9aa1832aa2023c559a8abd6ef4155e3f
luchezarIvanov <luchobg@abv.bg>
2016-07-05 15:31:02 +0300

    corrected size

--- app/src/main/java/com/example/lachezarivanov/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lachezarivanov/myapplication/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.lachezarivanov.myapplication;
 
-import android.content.Context;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,18 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
-import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity
