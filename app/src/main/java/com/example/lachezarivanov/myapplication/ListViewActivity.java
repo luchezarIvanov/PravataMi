e0f0b3bf389f4347e019c9246ead61b4ff9236a9
luchezarIvanov <luchobg@abv.bg>
2016-07-06 17:12:36 +0300

    initiated webview but with hardcoded page

--- app/src/main/java/com/example/lachezarivanov/myapplication/ListViewActivity.java
+++ app/src/main/java/com/example/lachezarivanov/myapplication/ListViewActivity.java
@@ -1,11 +1,13 @@
 package com.example.lachezarivanov.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ListViewActivity extends AppCompatActivity {
@@ -20,6 +22,15 @@ public class ListViewActivity extends AppCompatActivity {
         ListView listview = (ListView) findViewById(R.id.listview);
         listview.setAdapter(new ImageAdapter2(this));
 
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                SendToWebView();
+              /*  Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show(); */
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +41,11 @@ public class ListViewActivity extends AppCompatActivity {
         });
     }
 
+    public void SendToWebView() {
+        Intent intent = new Intent(this, WebActivity.class);
+        startActivity(intent);
+    }
+
 }
+
+
--- app/src/main/java/com/example/lachezarivanov/myapplication/WebActivity.java
+++ app/src/main/java/com/example/lachezarivanov/myapplication/WebActivity.java
@@ -1,11 +1,16 @@
 package com.example.lachezarivanov.myapplication;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -15,6 +20,22 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient(){
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request.getUrl().toString());
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        myWebView.loadUrl("http://www.pravatami.bg/10063");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
